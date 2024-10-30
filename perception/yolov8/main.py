8526a3976e91dfcea4ad0ebee7bb45d03e70ece1
ehbnr <33774925+ehbnr@users.noreply.github.com>
2024-10-30 21:06:33 +1100

    fsoco trained model, use best.pt model

--- perception/yolov8/main.py
+++ perception/yolov8/main.py
@@ -2,13 +2,19 @@ from ultralytics import YOLO as yolo
 import cv2
 
 file_path = './data/track_vid.mp4'
+file_path2 = './data/amz_run1.mp4'
+file_path3 = './data/av_run1.mp4'
+file_path4 = './data/av_run2.mp4'
 
-cap = cv2.VideoCapture(file_path)
+# cap = cv2.VideoCapture(file_path)
+# cap = cv2.VideoCapture(file_path2)
+cap = cv2.VideoCapture(file_path3)
+# cap = cv2.VideoCapture(file_path4)
 
-model = yolo("yolov8m.pt")
+model = yolo("best.pt")
 
 # results = model.train(data='coco128.yaml', epochs=1, batch=-1)
-# results = model.train(data='/home/ab/git/datasets/fsoco/data.yaml', epochs=1, batch=-1)
+# results = model.train(data='/home/ab/git/2024_2025_av_sim_stack/perception/yolov8/data/datasets/fsoco/data.yaml', epochs=10, batch=-1)
 
 while cap.isOpened():
 
