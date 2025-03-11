53c671698ae0c438d922827d531f0b85f3387f56
Abner <flkdsff@sjlkfjads.local>
2025-03-11 11:47:43 +1100

    don't remember what the change was

--- perception/yolov8/main.py
+++ perception/yolov8/main.py
@@ -6,9 +6,9 @@ file_path2 = './data/amz_run1.mp4'
 file_path3 = './data/av_run1.mp4'
 file_path4 = './data/av_run2.mp4'
 
-# cap = cv2.VideoCapture(file_path)
+cap = cv2.VideoCapture(file_path)
 # cap = cv2.VideoCapture(file_path2)
-cap = cv2.VideoCapture(file_path3)
+# cap = cv2.VideoCapture(file_path3)
 # cap = cv2.VideoCapture(file_path4)
 
 model = yolo("best.pt")
